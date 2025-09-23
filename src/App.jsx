64266821d6001093e8584e4e6cc55fe9181a35a4
KidMaly1 <titouslef@gmail.com>
2025-09-23 13:48:25 +0200

    New commit September 23 2025

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ import Banner from './components/Banner.jsx'
 import Footer from './components/Footer.jsx'
 import Housing from './pages/Housing.jsx'
 import ErrorPage from './pages/Errorpage.jsx'
-import Carrousel from './components/Carrousel.jsx'
-import Dropdown from './components/Dropdown.jsx'
-import Collapse from './components/Collapse.jsx'
-import Tags from './components/Tags.jsx'
 
 
 function App () {
@@ -21,8 +17,6 @@ function App () {
             <Route path="/About" element={<About />} />
             <Route path="/Housing/:id" element={<Housing />} />
             <Route path="*" element={<ErrorPage/>} />
-            <Route path="/Dropdown" element={<Dropdown />} />
-            <Route path="/Collapse" element={<Collapse />} />
         </Routes>
       <Footer />
 
